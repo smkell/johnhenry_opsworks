9118334da6aace7e399e8530e1129920ea09376d
Kell, Sean <skell@ea.com>
2015-12-11 16:17:26 -0500

    Update johnhenry cookbook.

--- johnhenry/recipes/elasticsearch.rb
+++ johnhenry/recipes/elasticsearch.rb
@@ -1,6 +1,9 @@
 Chef::Log.info("********** Preparing to install and configure elasticsearch! **********")
 
-search("aws_opsworks_instance").each do |instance|
-  Chef::Log.info("********** The instance's hostname is '#{instance['hostname']}' **********")
-  Chef::Log.info("********** The instance's ID is '#{instance['instance_id']}' **********")
-end
+# search("aws_opsworks_instance").each do |instance|
+#   Chef::Log.info("********** The instance's hostname is '#{instance['hostname']}' **********")
+#   Chef::Log.info("********** The instance's ID is '#{instance['instance_id']}' **********")
+# end
+
+include_recipe 'java::default'
+include_recipe 'elasticsearch::default'
