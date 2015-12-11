8ecd60506d8ead20018dfab28f8c6b0b0b7c4cbe
Kell, Sean <skell@ea.com>
2015-12-11 15:54:23 -0500

    Initial commit.

---
+++ johnhenry/recipes/elasticsearch.rb
@@ -0,0 +1,6 @@
+Chef::Log.info("********** Preparing to install and configure elasticsearch! **********")
+
+search("aws_opsworks_instance").each do |instance|
+  Chef::Log.info("********** The instance's hostname is '#{instance['hostname']}' **********")
+  Chef::Log.info("********** The instance's ID is '#{instance['instance_id']}' **********")
+end
