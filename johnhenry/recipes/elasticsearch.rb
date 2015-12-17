7a2a8ad1dbd08d00497314cb3d4ca6cef4c2209c
Sean Kell <sean.michael.kell@gmail.com>
2015-12-17 11:37:22 -0500

    Update johnhenry

--- johnhenry/recipes/elasticsearch.rb
+++ johnhenry/recipes/elasticsearch.rb
@@ -5,7 +5,7 @@ Chef::Log.info("********** Preparing to install and configure elasticsearch! ***
 #   Chef::Log.info("********** The instance's ID is '#{instance['instance_id']}' **********")
 # end
 
-instances = 
+instances =
   begin
     search("aws_opsworks_instance", "NOT self:true")
   rescue Net::HTTPServerException, Chef::Exceptions::InvalidDataBagPath
@@ -26,18 +26,28 @@ end
 include_recipe 'java'
 elasticsearch_user "elasticsearch"
 elasticsearch_install 'elasticsearch'
-elasticsearch_configure 'elasticsearch' do 
+elasticsearch_configure 'elasticsearch' do
   path_data package: node['johnhenry']['elasticsearch']['path_data']
   configuration ({
     'network.host' => '0.0.0.0',
     'cluster.name' => node['johnhenry']['elasticsearch']['cluster_name'],
-    'discovery.zen.ping.multicast.enabled' => false,
-    'discovery.zen.unicast.hosts' => "[#{ips.join(',')}]"
+    'cloud.aws.access_key' => 'AKIAI5OEQBVK4CVU5AHA',
+    'cloud.aws.secret_key' => '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',
+    'plugin.mandatory' => 'cloud-aws',
+    'discovery.type' => 'ec2',
+    'discovery.ec2.host_type' => 'public_ip',
+    'discovery.ec2.ping_timeout' => '30s',
+    'discovery.ec2.availability_zones' => 'us-west-2a',
+    'cloud.aws.region' => 'us-west',
+    'discovery.zen.ping.multicast.enabled' => false
   })
 end
 
 elasticsearch_service 'elasticsearch'
 
-service 'elasticsearch' do 
+elasticsearch_plugin 'elasticsearch/elasticsearch-cloud-aws'
+elasticsearch_plugin 'lmenezes/elasticsearch-kopf'
+
+service 'elasticsearch' do
   action :start
 end
